67faed667dc0dd2bf4451ce2e1c3949ac515075e
HasanAlQaisi <hasan.alqaisi2000@gmail.com>
2020-11-10 20:11:01 +0300

    add constatns

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,11 @@
+// Widgets constants
+const kHeroAnimLogoTag = 'logo hero Tag';
+
+// Strings constants - LocalValidators
+const enterEmailMessage = 'Please Enter your email';
+const enterValidEmailMessage = 'Enter a valid email';
+
+const enterPasswordMessage = 'Please Enter your password';
+
+const enterUsernameMessage = 'Enter a username';
+const spacesInUsernameMessage = 'Username shoudn\'t contain spaces';
