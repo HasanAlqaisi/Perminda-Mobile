908f813b64df9ba18286428b0857dd9434670156
HasanAlQaisi <hasan.alqaisi2000@gmail.com>
2020-11-10 20:11:54 +0300

    edit login design

--- lib/presentation/features/login/pages/login_screen.dart
+++ lib/presentation/features/login/pages/login_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:perminda/core/global_widgets/global_widgets.dart';
+import 'package:perminda/core/validators/local/local_validators.dart';
 import 'package:perminda/presentation/features/login/widgets/widgets.dart';
 
 class LoginScreen extends StatelessWidget {
+  static const route = '/';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,9 +41,22 @@ class _LoginFormState extends State<LoginForm> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             LogoImage(),
-            EmailField(),
-            PasswordField(),
-            LoginButton(),
+            RectangleTextField(
+              hintText: 'Enter your email',
+              prefixIcon: Icons.email,
+              validateRules: (value) {
+                return LocalValidators.emailValidation(value);
+              },
+            ),
+            SizedBox(height: 30.0),
+            PasswordField(hintText: 'Enter your password'),
+            ForgotPasswordButton(),
+            RectangleButton(
+              text: 'Login',
+              onPressed: () {
+                //TODO: Make a logining request
+              },
+            ),
             RegisterButton(),
           ],
         ),
