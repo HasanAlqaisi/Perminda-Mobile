5ee86c049d0b52380469952c82a8040b151ab62f
HasanAlQaisi <hasan.alqaisi2000@gmail.com>
2020-11-12 02:52:54 +0300

    registration usecase added

---
+++ lib/domain/usecases/auth/register_user.dart
@@ -0,0 +1,21 @@
+import 'package:dartz/dartz.dart';
+import 'package:perminda/core/errors/failure.dart';
+import 'package:perminda/data/remote_models/auth/user.dart';
+import 'package:perminda/domain/repos/auth_repo.dart';
+
+class RegisterUserUseCase {
+  final AuthRepo authRepo;
+
+  RegisterUserUseCase({this.authRepo});
+
+  Future<Either<Failure, User>> call(
+    String firstName,
+    String lastName,
+    String username,
+    String email,
+    String password,
+  ) async {
+    return await authRepo.registerUser(
+        firstName, lastName, username, email, password);
+  }
+}
