8f6b9724901e7dcd5d11386dab4b4d6175ee0192
HasanAlQaisi <hasan.alqaisi2000@gmail.com>
2020-11-12 16:39:12 +0300

    main edited

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:perminda/presentation/features/login/pages/login_screen.dart';
 import 'package:perminda/presentation/features/registration/pages/register_screen.dart';
+import 'injection_container.dart' as di;
 
 void main() {
+  di.init();
   runApp(Perminda());
 }
 
@@ -10,7 +12,6 @@ class Perminda extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'This is the start point of the app',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
