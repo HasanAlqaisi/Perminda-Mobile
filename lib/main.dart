2aec860289843fdab93e4be28bbab7dd5080643f
HasanAlQaisi <hasan.alqaisi2000@gmail.com>
2020-11-13 20:21:27 +0300

    add forogot password screen route

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:perminda/presentation/features/forgot_password/pages/forgot_password_screen.dart';
 import 'package:perminda/presentation/features/login/pages/login_screen.dart';
 import 'package:perminda/presentation/features/registration/pages/register_screen.dart';
 import 'injection_container.dart' as di;
@@ -20,6 +21,7 @@ class Perminda extends StatelessWidget {
       routes: {
         LoginScreen.route: (context) => LoginScreen(),
         RegisterScreen.route: (context) => RegisterScreen(),
+        ForgotPassScreen.route: (context) => ForgotPassScreen(),
       },
     );
   }
