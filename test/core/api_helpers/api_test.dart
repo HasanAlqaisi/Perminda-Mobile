6d26d2b1b7aa3a720897675fab0d29758b62cf77
HasanAlQaisi <hasan.alqaisi2000@gmail.com>
2020-11-24 02:44:44 +0300

    orderUrlProvider tested

--- test/core/api_helpers/api_test.dart
+++ test/core/api_helpers/api_test.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:perminda/core/api_helpers/api.dart';
+import 'package:perminda/core/constants/sensetive_constants.dart';
+import 'package:perminda/data/remote_models/orders/order_params.dart';
 
 void main() {
   final url = 'http://api.example.org/accounts/?offset=400&limit=100';
@@ -13,6 +15,39 @@ void main() {
     test('should return [int]offset if url is valid', () {
       final result = API.offsetExtractor(url);
       expect(result, 400);
+    });
+  });
+
+  group('orderUrlProvider', () {
+    test('should provide correct url if list has no items', () {
+      final correctUrl = '$baseUrl/api/order/';
+      List<OrderParams> orderParams = [];
+
+      final result = API.orderUrlProvider(orderParams);
+
+      expect(result, correctUrl);
+    });
+
+    test('should provide correct url if list has one item', () {
+      final correctUrl = '$baseUrl/api/order/?product0=1&quantity0=50';
+      var orderParams = [OrderParams(productid: '1', quantity: 50)];
+
+      final result = API.orderUrlProvider(orderParams);
+
+      expect(result, correctUrl);
+    });
+
+    test('should provide correct url if list has more than item', () {
+      final correctUrl =
+          '$baseUrl/api/order/?product0=1&quantity0=50&product1=2&quantity1=10';
+      var orderParams = [
+        OrderParams(productid: '1', quantity: 50),
+        OrderParams(productid: '2', quantity: 10),
+      ];
+
+      final result = API.orderUrlProvider(orderParams);
+
+      expect(result, correctUrl);
     });
   });
 }
